5cc0d00264d60184c0b16b43ab996c8f9762e32a
Administrator <21321313>
2018-12-28 09:14:40 +0800

    123

---
+++ profile.php
@@ -0,0 +1,6 @@
+<?php
+header("Access-Control-Allow-Origin;http://10.30.29.90:63341");//前端ip和端口
+header("Access-Control-Allow-Credentials;true");
+$userName=array('userName'=>'123456');
+echo json_encode($userName);
+?>
