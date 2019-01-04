20afe8ce7da6e7303fe0f932a23deffd0057708f
17002090139章洪 <43225073+Z17002090139@users.noreply.github.com>
2019-01-04 08:53:38 +0800

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,4 +1,5 @@
 <?php
+header('Content-Type:application/json; charset=utf-8');
 header("Access-Control-Allow-Origin;http://10.30.29.90:63341");//前端ip和端口
 header("Access-Control-Allow-Credentials;true");
 $userName=array('userName'=>'123456');
